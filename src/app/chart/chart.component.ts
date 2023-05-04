a721b24f9902a96a855137a4493c625827b267a5
cooperhc <69803510+cooperhc@users.noreply.github.com>
2023-05-03 23:44:06 -0400

    Changed how chart page loaded visualizations

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -29,63 +29,78 @@ export class ChartComponent implements OnInit{
   nvda = false
   assets = false
   load = true
+  selectedChart = ""
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') as string
     if(this.id==="1"){ 
       this.usa = true
+      this.selectedChart = "usa"
     }
     else if(this.id==="2"){
       this.uk = true
+      this.selectedChart = "uk"
     }
     else if(this.id=="3"){
       this.shc = true
+      this.selectedChart = "shc"
     }
     else if(this.id=="4"){
       this.nkx = true
+      this.selectedChart = "nkx"
     }
     else if(this.id=="5"){
       this.dax = true
+      this.selectedChart = "dax"
     }
     else if(this.id=="6"){
       this.moex = true
+      this.selectedChart = "moex"
     }
     else if(this.id=="7"){
       this.amzn = true
+      this.selectedChart = "amzn"
     }
     else if(this.id=="8"){
       this.amd = true
+      this.selectedChart = "amd"
     }
     else if(this.id=="9"){
       this.aapl = true
+      this.selectedChart = "aapl"
     }
     else if(this.id=="10"){
       this.googl = true
+      this.selectedChart = "googl"
     }
     else if(this.id=="11"){
       this.meta = true
+      this.selectedChart = "meta"
     }
     else if(this.id=="12"){
       this.msft = true
+      this.selectedChart = "msft"
     }
     else if(this.id=="13"){
       this.nflx = true
+      this.selectedChart = "nflx"
     }
     else if(this.id=="14"){
       this.nvda = true
+      this.selectedChart = "nvda"
     }
     else if(this.id=="15"){
       this.tsla = true
+      this.selectedChart = "tsla"
     }
     else if(this.id=="16"){
       this.btc = true
+      this.selectedChart = "btc"
     }
     else if(this.id=="17"){
       this.eth = true
-    }
-    else if(this.id=="18"){
-      this.assets = true
-    }    
+      this.selectedChart = "eth"
+    }  
     window.scrollTo(0, 0);
     
   }
